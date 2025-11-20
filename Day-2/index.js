ccd3e25d65b0b8027cef1f05898d354bac0fd2b4
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2025-11-20 16:26:10 +0530

    Day-2 JS

---
+++ Day-2/index.js
@@ -0,0 +1,87 @@
+//this sing line comments
+/* 
+multiline comments
+
+*/
+/**
+ * 
+ * documentation comments
+ * 
+ */
+
+// console.log(1);
+// console.log(2);
+
+// setTimeout(()=>{
+//     console.log(3);
+    
+// },2000);
+
+// console.log(4);
+// console.log(5);
+
+
+/**
+ * Variables:-
+ * 
+ * Variables are acts as a container to store the Values(Data Types);
+ * 
+ * diff types:-
+ * 
+ * Var
+ * let 
+ * const
+ * automatic
+ * 
+ * sy:-
+ * 
+ * Variable identifier = Value;
+ * 
+ */
+
+// var a=34;//initilization
+// console.log(a);
+// a=43;//assignment
+// console.log(a);
+// var a;//diclaration
+// console.log(a);
+
+/**
+ * 
+ * 
+ * 
+ */
+
+var _name="Arthik";
+console.log(name);//error, arthik,error
+console.log(_name);
+// console.log(_Name);
+
+var Name="Javascript";
+console.log(name);// error space
+// console.log(_Name); //error
+console.log(Name); //Javascript
+
+var $123=123;
+console.log(123);
+
+// var _Abc$"="Hello There"
+// Console.log(_Abc$")
+
+// var AbcX123="Hello There"
+// console.log(Abcx123)
+
+
+// var const="Suresh";
+// console.log(const) 
+
+
+
+
+
+
+
+
+
+
+
