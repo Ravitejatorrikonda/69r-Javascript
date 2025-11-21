a66a97e54488ccb595b96ed3285f6e18f3822122
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2025-11-21 16:31:03 +0530

    GEC Datatypes in js

---
+++ Day-3/index.js
@@ -0,0 +1,115 @@
+/**
+ * 
+ * 
+ */
+
+
+
+/**
+ * 
+ * Hoisting is a process of accessing a values before diclaration is known as hoisting
+ * //undefined
+ * 
+ * GEC:-
+ * -----
+ * GEC stands for global exicution context
+ * it is a starting and ending point of our javascript
+ * inside the function code FEC will be take care
+ * Out side the function code will be take care by the GEC
+ * 
+ * in this Global exicution context has a two phases
+ * 
+ * ->Memory phase
+ * ->Exicution phase
+ * ->at the time memory phase variable var can be diclared as undifined and window object created and this keyword created
+ * 
+ */
+
+// console.log(a);//Hoisting
+// var a=10;
+// console.log(a);
+
+
+// var a=34;
+// a=43;
+// var a;
+// console.log(a);
+
+// console.log(a,b,c);//undefined
+// var a=10;
+// console.log(a);//10
+// var b=20;
+// console.log(c);//un
+// console.log(b);//20
+// var c=b+10;
+// console.log(c);//30
+
+// //un, 10, un, 20, 30
+
+// console.log(a);//un
+// var a=10;
+// console.log(b+a);
+// var b=20;
+// console.log(c);//un
+// var c=b+100;
+// console.log(c);//120
+// console.log(b);//20
+
+
+/**
+ * 
+ * data types in javascript:-
+ * --------------------------
+ * 
+ * Data types are container which is used to store the type data, to perform a specific task,
+ * two types of datatypes:-
+ * primitive:-
+ * -----------
+ * Primitive data types are immutable(once data can be assign to a variable can not be changes)
+ * 
+ *  types:-
+ * 
+ * ->number
+ * ->String
+ * ->boolean
+ * ->undefined
+ * ->null
+ * 
+ * Non-primitive
+ * -------------
+ * Non-primitive datat types are muttable(once data can be assign, it is possible to change)
+ * 
+ * types:-
+ *        arrays
+ *        objects
+ *        functions
+ *        set
+ *        etc.,
+ * 
+ * 
+ */
+
+// var a=null;
+// console.log(typeof(a));
+
+// var a="";
+// console.log(a);
+
+// console.log(a);
+// var a=10;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
