e3d82b3d660cb7283b6798e94e8a729795735356
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2025-11-22 16:03:55 +0530

    Operators

---
+++ Day-4/index.js
@@ -0,0 +1,123 @@
+/**
+ * 
+ * 
+ * data types
+ * GEC
+ * 
+ * Operatores:-
+ * -----------
+ * operators are a special symbols is used to perform a specific operations
+ * Types:-
+ * 
+ * ->Aerthamatic operatore
+ * ->assignment operatore
+ * ->logical operatore
+ * ->Ternary operatore
+ * 
+ * 
+ * Aerthamatic operatore:-
+ * ----------------------
+ *          + , -, *, /, %, ++, --
+ */
+
+// var sum=10;
+// console.log(sum+10);
+// console.log(sum- 20);
+
+// var a=34;
+// console.log(--a + a--);//70
+// console.log(a);//36
+
+// var score=100;
+// score +=4;//score =score+4
+// console.log(score);
+
+// var wallet=1000;
+// wallet -=400;
+// console.log(wallet);
+
+// logical operatore:-
+// ------------------
+
+/**
+ * Comparision operatore:-
+ * -------------------------
+ * 
+ * <, >, <=, >=, ==, ===, !=
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+// // var age="18";
+// // console.log(age === 18 );
+
+// var age=prompt("Enter a number")//
+
+// console.log(age >= 18);
+
+/**
+ * Logical operatore
+ * ------------------
+ * 
+ * && || !
+ */
+
+// console.log(true && true);//t
+// console.log(true && false);//f
+// console.log(false && true);//f
+// console.log(false && false);//f
+
+// console.log(true || true);//t
+// console.log(true || false);//t
+// console.log(false || true);//t
+// console.log(false || false);//f
+
+// console.log(true+true);//2
+// console.log(true+false);//1
+// console.log(false+true);//1
+// console.log(false+false);//0
+
+// var sum=prompt("ENter a number")
+// console.log(sum+10);
+
+// Boolean()
+// Number()
+// String()
+
+// var a=Boolean(0)
+// var b=true;
+// console.log(a+b);
+
+// var sum=+prompt("Enter a number")
+// // console.log(Number(sum)+Number(10));
+// console.log(sum+10);
+
+// console.log(!true);
+
+/**
+ * variable identifier=(condtion)? true value: False Value;
+ * 
+ */
+
+var age=+prompt("Enter age")
+var data=(age>=18)?"adult":"child"
+console.log(data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
