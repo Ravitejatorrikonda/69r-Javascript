3b4b0ad6ae06a95f19c7f49da3d0b28bc61b7b56
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2025-11-24 16:23:11 +0530

    Conditional statements

---
+++ Day-5/script.js
@@ -0,0 +1,150 @@
+/**
+ * 
+ * 
+ * Conditional statements in javascript
+ * ---------------------------------------
+ * conditional statements is used to control the flow of exicution, and it exicute the block of code.
+ * 
+ * diff types of conditional  statements
+ * 
+ * ->simple if
+ * ->if else
+ * ->ladder if
+ * ->nested if
+ * ->Switch case
+ * 
+ * ->simple if:-
+ * 
+ *     if(condition){
+ * //statements
+ * }
+ * 
+ * 
+ */
+// debugger;
+// var mounikaLaptop=true;
+// if(mounikaLaptop){
+// console.log("Attending the class");
+
+// }
+// if(!mounikaLaptop){
+//     console.log("not attending the class");
+// }
+
+// var grade="A";
+// var marks=75;
+
+// // if((grade === "A" || grade === "B") && marks >= 75){
+// //     console.log("Eligible");
+    
+// // }
+// // if(!(grade === "A" || grade === "B") && marks >= 75){
+// //     console.log("Not ELigible");
+    
+// // }
+
+// // if((grade === "A" || grade === "B") && marks >= 75){
+// //     console.log("Eligible");
+    
+// // }else{
+// //     console.log("Not eligible");
+    
+// // }
+
+// /**
+//  * Write a javascript code ,
+//  * if age is below 13 ->child
+//  * if age is 13 or above 13 ->teenage->18 or 18 below
+//  * if age is above 18 adult;
+//  * 
+//  */
+
+// // var age=+prompt("Enter a number")
+
+// // if(age < 13){
+// //     console.log("Child");
+    
+// // }
+// // else if(age >= 13 && age <=18){
+// //     console.log("Teenage");
+    
+// // }
+// // else{
+// //     console.log("adult");
+    
+// // }
+// // else{
+// // console.log("not a valid age");
+
+// // }
+
+// var marks=110;
+// if(marks >= 90 && marks<= 100){
+//     console.log("A");
+    
+// }
+// else if(marks >= 80 && marks < 90){
+// console.log("B");
+
+// }
+// else if(marks >=70 && marks < 80){
+//     console.log("C");
+
+    
+// }
+// else if(marks >=0 && marks <70){
+//     console.log("Fail");
+    
+// }
+// else{
+//     console.log("Not a valid marks");
+    
+// }
+
+
+// var country="india";
+
+// if(country == "india"){
+//     var gender="male";
+//     if(gender == "male"){
+//         console.log("Male");
+        
+//     }else{
+//         console.log("Female");
+        
+//     }
+    
+// }else{
+//     console.log("not a indian");
+    
+// }
+
+
+/**
+ * 
+ * switch (expression){
+ * case value://statements
+ * break;
+ * case value://statements
+ * break;
+ * .......................
+ * default: //statements
+ * 
+ * }
+ * 
+ * 
+ */
+
+var day=10
+
+// switch(day){
+//     case 1:console.log("sunday");break;
+//     case 2:console.log("monday");break;
+//     case 3:console.log("tuesday");break;
+//     case 3:console.log("wednsday");break;
+//     default:console.log("Weekand");
+    
+    
+    
+// }
+
