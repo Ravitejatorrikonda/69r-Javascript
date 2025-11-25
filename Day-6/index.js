2857dbe9b00c0adf8e35fdca5db2df55cd1dc836
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2025-11-25 16:34:48 +0530

    Loops

---
+++ Day-6/index.js
@@ -0,0 +1,113 @@
+/**
+ * 
+ * COnditional statements:-
+ * simple if
+ * if else
+ * ladder if
+ * nested if
+ * switch case
+ * 
+ * 
+ * Loops in Javascript
+ * -------------------
+ * loops is used to exicute the block of code repeatdly until the condition met.
+ * 
+ * diff types of loops
+ * ->For loop
+ * ->while loop
+ * ->do while
+ * ->for of
+ * ->for in
+ * 
+ * for loop:-
+ * ----------
+ * sy:-
+ *  for(initilization;condition;updation){
+ * //statements
+ * }
+ * 
+ */
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+/**
+ * 
+ * ->When i run the for loop 
+ * ->first step
+ *    ->intilization
+ *    ->condition
+ *  if condition is false, the block of code will not be exicuted
+ * if condition id True, the block of code will be exicuted
+ * ->second time itration:-
+ *     ->condition
+ *     ->updation
+ * 
+ * for(intilization; condition; updation){
+ * 
+ * 
+ * }
+ * 
+ * 
+ * 
+ */
+
+// for(i=1;i<=10;i++){
+//     console.log(i)
+// }
+
+
+// for(i=10; i>=1;i--){
+//     console.log(i);
+
+// }
+
+// for (i = 1; i != 11; i++) {
+//     console.log(i);
+
+// }
+
+/**
+ * 
+ * Write a javascript code to find the even numbers 1 to 200 using for loop and  conditional statements
+ */
+
+// for (i = 1; i <= 200; i++) {
+//     if (i % 2 != 0) {
+//         console.log(i);
+
+
+//     }
+// }
+
+// var sum="";
+// for(i=1; i<=20;i++){
+//     sum +="sorry";
+// }
+// console.log(sum);
+
+/**
+ * sy:-
+ * 
+ * while(condition){
+ * //statements
+ * //updation
+ * }
+ * 
+ * 
+ */
+
+var i=1;
+while(i<=10){
+    console.log(i);
+    i++;
+}
+
