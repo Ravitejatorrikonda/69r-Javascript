4d39402321be5e16584c8f08902cec8e95489b61
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2025-11-26 16:24:44 +0530

    FUnctions-1

---
+++ Day-7/index.js
@@ -0,0 +1,178 @@
+/**
+ * 
+ * 
+ * What is Javascript?
+ * v8 Engine
+ * Datatypes
+ * Variables
+ * GEC
+ * Operators
+ * Conditional Statements
+ * Loops:-
+ * 
+ * FUnctions In Javascript:-
+ * --------------------------
+ *    Functions are re-usable block of code. it can be used to orgnise our code, and also we cs use any no of times
+ * 
+ * Note:-   Named functions can be hoisted
+ * 
+ * Diff types:-
+ * ------------
+ * ->Named functions
+ * ->First class FUnction
+ * ->Arrow functions
+ * ->Anonymous function
+ * ->Constructore function
+ * ->Call Back FUnctions
+ * ->Higher order function
+ * ->Lower Order function
+ * ->Pure functions
+ * ->impure functions
+ * ->Default parameter funtions
+ * ->Optional parameter function
+ * ->Rest parameter function
+ * ->Immediate invoked functions
+ * ->Generatore functions
+ * 
+ * 
+ * Named functions:-
+ * -------------------
+ * 
+ * SY:-
+ * 
+ *  function difination:-
+ * ----------------------
+ *                  function FUnction_Name([Parameters]){
+ *                                  //statements
+ *                                [return Value]
+ *                        }
+ * Note:-
+ * -------
+ * Function Defination is always stored in function Name
+ * 
+ * 
+ * FUnction calling:-
+ * ------------------
+ * function_Name([arguments])
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+// function sayHi(){
+//     console.log("Hello There");
+    
+// }
+// sayHi();
+
+
+// function kitchen(){
+//     console.log("Enterd into Kichen");
+//     console.log("started food preparation");
+//     delivery();
+//     console.log("food Completed");
+//     console.log("Exit to kitchen");
+    
+    
+    
+// }
+// function delivery(){
+//     console.log("Taking Delivery");
+
+    
+// }
+// kitchen();
+
+
+// function one(){
+//     console.log("Started");
+//     console.log("Ended");
+    
+    
+// }
+// one()
+// one()
+// one()
+// one()
+
+// function one(){
+//     console.log("One");
+//     two();
+//     console.log("two"); 
+// }
+// function two(){
+//     console.log("Two");
+  
+//     console.log("Three");    
+// }
+// function three(){
+//     console.log("Three");
+//     one()
+//     console.log("One");   
+// }
+// two()
+// three()
+
+
+// function sum(a, b){
+//     console.log(a+b);
+    
+// }
+// sum(10, 20);
+
+
+// function sum(a){
+//     console.log(a+10);
+    
+// }
+// sum(10)
+
+function getData1(a,b,c){
+    console.log(a+b+c);
+    getData2(a,c)
+
+    
+}
+function getData2(a,b){
+    console.log(a+b);
+    
+}
+getData1(10,20,30)
+
+// function sum(a,b,c){
+//     console.log(a+b+c);
+//     console.log(a);
+//     console.log(c);
+    
+    
+    
+
+// }
+// sum(10,20,30)
+
+
+
+function printData(a,b,c){
+    console.log(a);
+    console.log(b);
+    
+    
+}
+printData()
+
+
+// var a;
+// console.log(a);
+
+
+
+
+
+
+
+
+
+
+
