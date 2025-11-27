6e405f74391f6d90dce80df4f61951af7f04c3aa
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2025-11-27 16:20:14 +0530

    Named functions-2

---
+++ Day-8/index.js
@@ -0,0 +1,108 @@
+/**
+ * 
+ * 
+ * Parameters, arguments, return
+ * 
+ */
+
+// function print(data){
+//     console.log(data)
+// }
+// print("hello There")
+
+
+// function maggie(maggie){
+// console.log(`remove ${maggie} maggie`);
+
+// }
+// maggie("2 packets ")
+
+// function one(a,b,c){
+//     console.log(a+b,a);//
+//     two(a+b,b)
+//     console.log("one completed");
+    
+    
+// }
+// function two(a,b){
+// console.log(a*b);
+
+// }
+// one(10, 2, 30)
+
+// function x(a,b,c){
+//     console.log("x function");
+// y(a,b)
+// z(a+b,c)
+// }
+// function y(a){
+//     console.log("y function");
+//     console.log(a+10);  
+// }
+// function z(b){
+//     console.log("z function");
+//     console.log(b+20);   
+// }
+// x(10, 20, 30)
+
+
+
+
+// function Devara(hero, maincharcter){
+//     console.log("Devara");
+//     console.log(hero+maincharcter);
+//     pushpa(hero, maincharcter)
+//     console.log("completed ");
+//     pushpa("Allu", "Pawan")
+    
+// }
+// function pushpa(hero, villan){
+//     console.log("pushpa");
+//     console.log(hero+villan);
+    
+    
+// }
+// Devara("NTR", "Prakash")
+
+
+// function sayHi(a){
+//     return "a"+10;
+// }
+// var result=sayHi(10);
+// console.log(result);
+
+
+
+// function sum(){
+//     var a=10;
+//     console.log(a);//10
+    
+// }
+// sum()
+// console.log(a)//not defined
+
+// function x(){
+//     console.log(a)
+//     return "hello there"
+//     var a=10;
+// }
+// x();
+
+
+// console.log(a)//un
+// console.log(demo1())//un,
+// var a=10;
+// function demo1(){
+//     console.log(a)
+// }
+// demo1()
+
+/**
+ * 
+ * 
+ * parameters arguments  return FEC,
+ * 
+ * 
+ * 
+ */
+
