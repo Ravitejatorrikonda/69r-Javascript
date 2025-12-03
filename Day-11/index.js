ca419ecdf064925c72ae321c2f498d0dd6a30443
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2025-12-03 14:56:40 +0530

    Scopes

---
+++ Day-11/index.js
@@ -0,0 +1,99 @@
+/**
+ * 
+ * anonymous fun
+ * arrow fun
+ * 
+ * 
+ * 
+ */
+
+// function sum(val,callback){
+//     console.log(val+10);//20
+//     callback(val)
+    
+// }
+// sum(10,val=>console.log(val))
+
+// function data(){
+//     var a=10;
+//     console.log(a);
+    
+// }
+
+
+
+
+// var a=10
+
+// var a=34;
+
+
+// function sum(){
+//     console.log(a)
+// }
+// sum()
+// if(true){
+//     console.log(a)
+// }
+// console.log(a)
+
+
+
+
+
+
+// if(true){
+    
+//     var a=43;
+//     console.log(a)
+// }
+// function sum(){
+//     console.log(a)
+// }
+// sum()
+
+// console.log(a)
+
+// var a=10;//init
+// a=30;
+// a=43;//re assign
+// var a;
+// var a=20;//re-inti
+// var a;
+// console.log(a)
+
+// let a=34;
+
+// a=43;
+// a=53;
+// // let a=20;
+// // let a;
+// console.log(a);
+
+
+// const siri=34;
+// const siri=43;
+// console.log(siri);
+
+// const x=function (){
+//     console.log("hello there")
+// }
+// x();
+// const x=function (){
+//     console.log("hello there")
+// }
+// x();
+
+
+
+
+// function printData1(fundef){
+//     console.log("Hello There 1")
+//     fundef()
+// }
+// function printData2(){
+//     console.log("Hello There 2")
+// }
+
+// printData1(printData2)
+
