af365397378eced0cf01c7840a0ce8bb71a343a1
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2025-12-11 15:31:15 +0530

    DELETE

---
+++ Day-16/index.js
@@ -0,0 +1,52 @@
+/**
+ * 
+ * https://dummyjson.com/quotes
+ * 
+ * https://dummyjson.com/todos
+ * 
+ * https://jsonplaceholder.typicode.com/photos
+ * 
+ * https://pokeapi.co/api/v2/pokemon/ditto
+ * 
+ * 
+ */
+
+
+// const URL = "https://dummyjson.com/todos";
+
+// fetch(URL)
+// .then((res)=>res.json())
+// .then((data)=>
+//     // for(i=0;i<=data.quotes.length;i++){
+//         console.log(data)
+
+
+// )
+
+// const URL="https://jsonplaceholder.typicode.com/photos"
+// fetch(URL)
+// .then(res=>res.json())
+// .then(data=>{
+//    for(let i=0;i<=data.length;i++){
+//     console.log(data[i]);
+
+//    }
+// })
+
+/**
+ * 
+ * GET,POST,PUT,PATCH,DELETE
+ * 
+ */
+
+// let options = {
+//     "method": "DELETE"
+// }
+// fetch("http://localhost:3000/products/3", options)
+//     .then(res => {
+//         if (res.ok) {
+//             console.log(res.status, res.statusText);
+
+//         }
+//     })
+
