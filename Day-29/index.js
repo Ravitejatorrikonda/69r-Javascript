bf3fdeec96bf25d0999417f110b61a61a0133421
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-01-07 17:15:43 +0530

    Clousers

---
+++ Day-29/index.js
@@ -0,0 +1,82 @@
+/**
+ * 
+ * create read delete update
+ * 
+ * 
+ * javascript:-
+ * datatypes,
+ * variables
+ * GEC
+ * FEC
+ * hoisting
+ * operators
+ * consitionals statements
+ * Loops
+ * functions
+ * callback hell
+ * scopes
+ * promises
+ * fetch
+ * array methods
+ * itrative methods
+ * json
+ * error handling
+ * async and await
+ * DOM->api calling,events,data fetch
+ * clousers,lixical scope regular expressions,
+ * 
+ * BOM->methods,local storeage,
+ * 
+ * 
+ * 
+ * 
+ */
+
+// function x(){
+//     let a=10;
+//     function y(){
+//         console.log(a)
+//     }
+//     y();
+    
+// }
+// x();
+
+// function x(){
+//     let a=10;
+//     function y(){
+//         console.log(a)
+//     }
+//     return y;
+// }
+// let b=x();
+// b();
+
+// let addCounter=(intialValue)=>{
+//     let counter=intialValue;
+
+//     return (countingValue)=>{
+//       counter +=countingValue;
+//       console.log(counter)
+//     }
+
+// }
+// let cart=addCounter(0);
+// cart(1)
+
+// for(i=1;i<=5;i++){
+//     setTimeout(()=>{
+//         console.log(i)
+//     },1000);
+// }
+
+
+// let reg=/hello/;
+// console.log(reg.test("hello"))
+
+// let reg=new RegExp(/hello/)
+// console.log(reg.test("hello"))
+
+// let reg=/javascript is awesome/
+// console.log(reg.mach("script"))
+
